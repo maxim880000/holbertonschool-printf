42dd7a3b005619f31d11163ac8ba19f816ea4655
maxim880000 <mdholberton@gmail.com>
2025-11-24 10:46:16 +0100

    feat(header): add main.h with printf prototype and required includes

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* library that we need to use variadic functions, write and malloc, free */
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+
+#endif
