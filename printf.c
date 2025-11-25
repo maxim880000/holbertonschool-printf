e23d2e3315e4df6ae45f4f9e004022eca970132b
CodexSC <carlos.alexandur@gmail.com>
2025-11-25 22:32:43 +0100

    Modified printf.c

--- printf.c
+++ printf.c
@@ -3,23 +3,64 @@
 
 /**
  * _printf - Prints formatted output to the standard output
- * @format: String containing the format specifiers 
- * 
+ * @format: String containing the format specifiers
+ *
  * Return: the number of characters or -1 if string(format) is NULL
  */
-int _printf(const char *format, ...);
+int _printf(const char *format, ...)
 {
-	va_list argument; /* we'll need it to recup all arguments */
-	int count; 		  /* to count all the caracters to display */
+	va_list args;
+	int count = 0;
+	int i = 0;
 
-	count = 0; 
-	if (!format) /* if format is NULL */	
-		return (-1); /* in case of error */
+	if (format == NULL)
+		return (-1);
 
-	va_start(argument, format); /* start the list of arguments */
+	va_start(args, format);
 
-	while (*format != '\0')
+	while (format[i])
 	{
-		
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else if (format[i] == 'c')
+			{
+				char c = va_arg(args, int);
+				_putchar(c);
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				char *s = va_arg(args, char *);
+				if (s == NULL)
+					s = "(null)";
+				while (*s)
+				{
+					_putchar(*s);
+					count++;
+					s++;
+				}
+			}
+			else
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				count += 2;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
 	}
+
+	va_end(args);
+	return (count);
 }
