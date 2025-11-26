25dc061d28943f6b5090c25524e3ee82a06839a9
CodexSC <carlos.alexandur@gmail.com>
2025-11-26 13:48:38 +0100

    Changed several filenames

---
+++ _printf.c
@@ -0,0 +1,65 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _printf - Prints formatted output to the standard output
+ * @format: String containing the format specifiers
+ *
+ * Return: the number of characters or -1 if string(format) is NULL
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+	int i = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else if (format[i] == 'c')
+			{
+				char c = va_arg(args, int);
+				_putchar(c);
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				char *s = va_arg(args, char *);
+				if (s == NULL)
+					s = "(null)";
+				while (*s)
+				{
+					_putchar(*s);
+					count++;
+				}
+			}
+			else
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				count += 2;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+
+	va_end(args);
+	return (count);
+}
