fd5b470227f757f13967051f5177b7878f7fff80
CodexSC <carlos.alexandur@gmail.com>
2025-11-26 15:01:40 +0100

    Accepted incoming change, replaced previous

--- main.h
+++ main.h
@@ -5,31 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-<<<<<<< HEAD
-/**
- * struct format - Struct format
- * @format: The format specifier character (c, s, % etc)
- * @f: Pointer to the function that handles this specifier
- *
- * Description: Structure that associates a format specifier
- * with its corresponding handler function
- */
-typedef struct format
-{
-	char *format;
-	int (*f)(va_list);
-} format_t;
-
 int _printf(const char *format, ...);
 int _putchar(char c);
 
-int print_char(va_list args);
-int print_string(va_list args);
-int print_percent(va_list args);
-
-=======
-int _printf(const char *format, ...);
-int _putchar(char c);
-
->>>>>>> testprintf
 #endif
